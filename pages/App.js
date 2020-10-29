460a27901ef0d3bcf881ec26c86cb75366dff008
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-29 15:10:39 +0300

    fetch the data

--- pages/App.js
+++ pages/App.js
@@ -1,5 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+const url = "https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10"
 function App() {
+  const [quote, setQuote] = useState([])
+  const [author, setAuthor] = useState("")
+
+  async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json()
+      setQuote(data);
+      console.log(data);
+  }
+
+  useEffect(() => {
+     fetchData()
+  }, [])
     return (
         <h1>Hello</h1>
     )
