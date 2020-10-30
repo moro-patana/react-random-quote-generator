0997e4e681ecd1db41d37b3bb4d002c820f63a3d
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-30 11:28:40 +0300

    removed the function fetch data by author name

--- pages/quotation.js
+++ pages/quotation.js
@@ -1,22 +1,8 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from 'react-router-dom'
 import { Link } from "react-router-dom"
 import refreshIcon from "../img/refresh.svg"
-// const authorLink = "https://quote-garden.herokuapp.com/api/v2/authors/"
-// const maxPage = "?page=1&limit=10"
 function Quotation({quoteText, quoteAuthor, quoteGenre}) {
-    // const { authorName } = useParams()
-    // const [author, setAuthor] = useState({})
 
-    // async function fetchAuthorName() {
-    //     const response = await fetch(authorLink + authorName + maxPage)
-    //     const dataAuthor = await response.json()
-    //     setAuthor(dataAuthor);
-    //     console.log(dataAuthor);
-    // }
-    // useEffect(() => {
-    //     fetchAuthorName()
-    // },[])
     const handleClick = (e) => {
         window.location.reload();
     }
