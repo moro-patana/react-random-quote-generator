5e1352f08e9fa8b538f6795effb17509689c32a7
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-30 15:55:34 +0300

    added className

--- pages/quotation.js
+++ pages/quotation.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import refreshIcon from "../img/refresh.svg"
-function Quotation({quoteText, quoteAuthor, quoteGenre}) {
+import arrowIcon from "../img/arrow-icon.svg"
+function Quotation({quoteText, quoteAuthor, quoteGenre, handleClick}) {
 
-    const handleClick = (e) => {
-        window.location.reload();
-    }
     const authorHandleClick = (e) => {
         // fetchAuthorName()
     }
     console.log(quoteAuthor);
     return (
-        <div>
-            <p><q>{quoteText}</q></p>
+        <div className="card-quotes">
             <div className="random">
                 <span>random</span>
                 <button onClick={handleClick} className="random-btn"><img src={refreshIcon} /></button>
             </div>
-            <Link to={`/authors/${quoteAuthor}`}>
-                <button className="author">
-                    <span>{quoteAuthor}</span>
-                    <span>{quoteGenre}</span>
-                </button>
+
+            <p><q> {quoteText} </q></p>
+            <div className="buttons">
+                <Link to={`/authors/${quoteAuthor}`}>
+                    <button className="author">
+                        <span>{quoteAuthor}</span>
+                        <span>{quoteGenre}</span>
+                        <img src={arrowIcon}/>
+                    </button>
                 </Link>
+            </div>
             </div>
     )
 }
