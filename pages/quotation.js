f17d62e3d77f86e0e4ffbae3169e3e3b65fce58d
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-30 11:28:25 +0300

    removed the function fetch data by author name

--- pages/quotation.js
+++ pages/quotation.js
@@ -1,9 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import refreshIcon from "../img/refresh.svg"
+// const authorLink = "https://quote-garden.herokuapp.com/api/v2/authors/"
+// const maxPage = "?page=1&limit=10"
 function Quotation({quoteText, quoteAuthor, quoteGenre}) {
+    // const { authorName } = useParams()
+    // const [author, setAuthor] = useState({})
+
+    // async function fetchAuthorName() {
+    //     const response = await fetch(authorLink + authorName + maxPage)
+    //     const dataAuthor = await response.json()
+    //     setAuthor(dataAuthor);
+    //     console.log(dataAuthor);
+    // }
+    // useEffect(() => {
+    //     fetchAuthorName()
+    // },[])
     const handleClick = (e) => {
         window.location.reload();
     }
+    const authorHandleClick = (e) => {
+        // fetchAuthorName()
+    }
+    console.log(quoteAuthor);
     return (
         <div>
             <h3><q>{quoteText}</q></h3>
@@ -11,11 +31,13 @@ function Quotation({quoteText, quoteAuthor, quoteGenre}) {
                 <span>random</span>
                 <button onClick={handleClick} className="random-btn"><img src={refreshIcon} /></button>
             </div>
-            <div className="author">
-                <p><a href="/">{quoteAuthor}</a></p>
-                <span>{quoteGenre}</span>
+            <Link to={`/authors/${quoteAuthor}`}>
+                <button className="author">
+                    <span>{quoteAuthor}</span>
+                    <span>{quoteGenre}</span>
+                </button>
+                </Link>
             </div>
-        </div>
     )
 }
 export default Quotation;
