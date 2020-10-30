06ceaf4ff7533e7a70d6f7a527ee1fdacfff65fe
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-30 11:24:38 +0300

    removed the fetchDataAuthor into another component

--- pages/App.js
+++ pages/App.js
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from "react"
 import Quotation from "./quotation.js"
+import QuotesByAuthor from "./quotesByAuthor.js"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 const url = "https://quote-garden.herokuapp.com/api/v2/quotes/random"
 function App() {
   const [quote, setQuote] = useState([])
-  const [author, setAuthor] = useState("")
 
   async function fetchData() {
       const res = await fetch(url);
       const data = await res.json()
       setQuote(data.quote);
-      console.log(data);
+      console.log(data.quote);
   }
- async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json()
-      setQuote(data.quote);
-      console.log(data);
-  }
-
   useEffect(() => {
      fetchData()
   }, [])
     return (
-        <Quotation {...quote}/>
+        <Router >
+            <Switch>
+                <Route path='/authors/:authorName'>
+                    <QuotesByAuthor/>
+                </Route>
+                <Route path='/'>
+                <Quotation {...quote}/>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 export default App;
